85ff2a1c9749e336a6d496345e3f6fe4e10b5da9
NealKumawat <nealkumawat04@gmail.com>
2025-04-07 06:41:02 +0000

    updated

---
+++ Basic/05_Strings.js
@@ -0,0 +1,199 @@
+// Lets look at the outdated string use
+let a = "Hello"
+let b = "Neal"
+console.log(a + " " + b + " How are You");
+
+
+// The new way for the same is:-
+console.log(`Everyone ${a}, my name is ${b}`)
+
+
+// There is one more way to declare string
+const GameName = new String('NealKumawat')
+
+// Lets use indesing
+console.log(GameName[0])
+console.log(GameName[2])
+console.log(GameName[3])
+
+
+//USING PROTOTYPES
+console.log(GameName.__proto__)
+
+console.log(GameName.length)    // #11
+console.log(GameName.toUpperCase()) // #NEALKUMAWAT
+console.log(GameName.charAt(3)) // #1
+console.log(GameName.indexOf('t'))  // #10
+console.log(GameName.substring(0, 5))   // #NealK
+console.log(GameName.slice(-10, 6))   // #ealKu
+// console.log(GameName.)
+
+
+
+
+
+
+
+/*
+PROTOTYPES
+
+anchor
+: 
+ƒ anchor()
+at
+: 
+ƒ at()
+big
+: 
+ƒ big()
+blink
+: 
+ƒ blink()
+bold
+: 
+ƒ bold()
+charAt
+: 
+ƒ charAt()
+charCodeAt
+: 
+ƒ charCodeAt()
+codePointAt
+: 
+ƒ codePointAt()
+concat
+: 
+ƒ concat()
+constructor
+: 
+ƒ String()
+endsWith
+: 
+ƒ endsWith()
+fixed
+: 
+ƒ fixed()
+fontcolor
+: 
+ƒ fontcolor()
+fontsize
+: 
+ƒ fontsize()
+includes
+: 
+ƒ includes()
+indexOf
+: 
+ƒ indexOf()
+isWellFormed
+: 
+ƒ isWellFormed()
+italics
+: 
+ƒ italics()
+lastIndexOf
+: 
+ƒ lastIndexOf()
+length
+: 
+0
+link
+: 
+ƒ link()
+localeCompare
+: 
+ƒ localeCompare()
+match
+: 
+ƒ match()
+matchAll
+: 
+ƒ matchAll()
+normalize
+: 
+ƒ normalize()
+padEnd
+: 
+ƒ padEnd()
+padStart
+: 
+ƒ padStart()
+repeat
+: 
+ƒ repeat()
+replace
+: 
+ƒ replace()
+replaceAll
+: 
+ƒ replaceAll()
+search
+: 
+ƒ search()
+slice
+: 
+ƒ slice()
+small
+: 
+ƒ small()
+split
+: 
+ƒ split()
+startsWith
+: 
+ƒ startsWith()
+strike
+: 
+ƒ strike()
+sub
+: 
+ƒ sub()
+substr
+: 
+ƒ substr()
+substring
+: 
+ƒ substring()
+sup
+: 
+ƒ sup()
+toLocaleLowerCase
+: 
+ƒ toLocaleLowerCase()
+toLocaleUpperCase
+: 
+ƒ toLocaleUpperCase()
+toLowerCase
+: 
+ƒ toLowerCase()
+toString
+: 
+ƒ toString()
+toUpperCase
+: 
+ƒ toUpperCase()
+toWellFormed
+: 
+ƒ toWellFormed()
+trim
+: 
+ƒ trim()
+trimEnd
+: 
+ƒ trimEnd()
+trimLeft
+: 
+ƒ trimStart()
+trimRight
+: 
+ƒ trimEnd()
+trimStart
+: 
+ƒ trimStart()
+valueOf
+: 
+ƒ valueOf()
+Symbol(Symbol.iterator)
+: 
+ƒ [Symbol.iterator]()
+*/
