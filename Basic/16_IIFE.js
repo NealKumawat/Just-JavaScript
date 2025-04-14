432694251437a77760c9d816565df786bbaa43ce
NealKumawat <nealkumawat04@gmail.com>
2025-04-14 11:11:35 +0000

    updated

--- Basic/16_IIFE.js
+++ Basic/16_IIFE.js
@@ -1,2 +1,24 @@
 /* IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE)
 This is used when u want to execute a funtion immediately while running a program*/
+
+// Below is an eg of Named IIFE
+(function chai(){
+    console.log("Database Connected");
+})(); // use semicolon
+
+//or
+
+// Below is an example of Unnamed IIFE
+( () =>{
+    console.log("DB connected");
+}) ();
+
+// let's see one more example
+
+// Let's pass parameter
+((name) => {
+    console.log(`DB connected ${name}`);
+}) ('Neal')
+
+// U just need to wrap ur funtion into ( ) and call it after defining immediately by ()
+// Imp: This is also used to treat the pollution caused by global scope
