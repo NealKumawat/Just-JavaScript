b32baa4d4e612b523ddbc194ecb76e8b65870612
NealKumawat <nealkumawat04@gmail.com>
2025-04-22 18:23:11 +0000

    updated

---
+++ Basic/Looping.js
@@ -0,0 +1,29 @@
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+
+
+let marr = ['Samosa', 'Kachori', 'Jalebi']
+let arr = 0
+while (arr < marr.length) {
+    console.log(`Value of my Arr is ${marr[arr]}`);
+    arr ++
+}
+
+
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score = score + 2
+} while (score <= 10);
+
+
+
+//ARRAY SPECIFIC LOOPS
+
+// for of loop
+const array = [1,2,3,4,5]
+foor (const num or object)
