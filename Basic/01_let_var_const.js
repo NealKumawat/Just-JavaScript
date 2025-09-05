32b79ff9f43557e92330b7f16bef82f18f179af6
NealKumawat <nealkumawat04@gmail.com>
2025-09-05 09:59:16 +0000

    updated

--- Basic/01_let_var_const.js
+++ Basic/01_let_var_const.js
@@ -1,10 +1,9 @@
 const Account_id = 344
 var Account_pass = "123@456"
-let User_State;
+let User_State;     // here User_State is an empty variable
 
 // var is not used now coz of the scope ("{}").
 // Initially if in this let us say Account_pass is made and assigned but if in a for loop the same variable is defined and changed, this would change the main variable at all places.
-// Above here User_State is an empty variable
 
 console.log(Account_id);
 console.log(Account_pass);
