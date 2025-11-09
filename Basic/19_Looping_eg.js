3ea42d8ffb336d444309687264a7405917eeb7e9
NealKumawat <nealkumawat04@gmail.com>
2025-11-09 10:38:59 +0000

    updated

---
+++ Basic/19_Looping_eg.js
@@ -0,0 +1,14 @@
+const books = [
+    { title: 'One', genre: 'Fiction', publish: 1981, edition: 2004},
+    { title: 'Two', genre: 'Non Fiction', publish: 1992, edition: 2008},
+    { title: 'Three', genre: 'History', publish: 1972, edition: 2005},
+    { title: 'Four', genre: 'Non Fiction', publish: 1938, edition: 2004},
+    { title: 'Five', genre: 'Science', publish: 1937, edition: 2023},
+    { title: 'Six', genre: 'Fiction', publish: 1974, edition: 2022},
+    { title: 'Seven', genre: 'History', publish: 1965, edition: 2006},
+    { title: 'Eight', genre: 'Science', publish: 1986, edition: 2019},
+    { title: 'Nine', genre: 'Non Fiction', publish: 1978, edition: 2024}
+]
+
+
+// Print details of the books whose genre is history
